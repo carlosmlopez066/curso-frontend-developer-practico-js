4d985fc327e2b268ca5d69fe00f6197f4aea339e
Carlos Lopez <carlosmlopez066@gmail.com>
2022-09-30 09:46:22 -0400

    Agregando el carrito de compras al documento principal. Aplicacion de logica para mostras y esconder los menu"

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
-const burguerMenu = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-burguerMenu.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu (){
-    desktopMenu.classList.toggle('inactive')
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    mobileMenu.classList.toggle('inactive')
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+
+    
+}
+
+function toggleCarritoAside() {
+    const ismobileMenuClose = mobileMenu.classList.contains('inactive');
+    
+    if (!ismobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+     aside.classList.toggle('inactive');
+
+   
 }
